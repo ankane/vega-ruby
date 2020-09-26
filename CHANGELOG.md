8e3c37365744d157b2c0d3459e064431f760eb37
Andrew Kane <andrew@chartkick.com>
2020-09-26 15:56:35 -0700

    Added repeat, resolve, spec, and selection methods

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,6 @@
 ## 0.1.1 (unreleased)
 
+- Added `repeat`, `resolve`, `spec`, and `selection` methods
 - Added support for Daru data frames
 
 ## 0.1.0 (2020-09-26)
--- lib/vega/lite_chart.rb
+++ lib/vega/lite_chart.rb
@@ -2,10 +2,10 @@ module Vega
   class LiteChart < BaseChart
     # https://vega.github.io/vega-lite/docs/spec.html
     scalar_methods \
-      :background, :padding, :autosize, :title, :name, :description, :width, :height, :mark
+      :background, :padding, :autosize, :title, :name, :description, :width, :height, :mark, :spec
 
     hash_methods \
-      :config, :usermeta, :projection, :datasets, :encoding
+      :config, :usermeta, :projection, :datasets, :encoding, :repeat, :resolve, :selection
 
     array_methods \
       :transform, :layer
