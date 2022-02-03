79453090769ee7db55eb807e96606fec925b8c3d
Andrew Kane <andrew@ankane.org>
2022-02-02 17:28:06 -0800

    Added nonce tests

--- test/helper_test.rb
+++ test/helper_test.rb
@@ -7,10 +7,25 @@ class HelperTest < Minitest::Test
     assert_match "vegaEmbed", vega_chart(Vega.lite)
   end
 
+  def test_nonce
+    stub(:content_security_policy_nonce, "test123") do
+      assert_match %{<script nonce="test123">}, vega_chart(Vega.lite)
+    end
+  end
+
+  def test_nonce_not_configured
+    refute_match "nonce", vega_chart(Vega.lite)
+  end
+
   def test_bad_type
     error = assert_raises(TypeError) do
       vega_chart Object.new
     end
     assert_equal "expected Vega chart", error.message
+  end
+
+  # for stubbing
+  def content_security_policy_nonce
+    nil
   end
 end
